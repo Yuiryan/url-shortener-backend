41dd1675c82a31eda0d8d2578281cd5e3991dbf7
Duc Nguyen Viet <duc.nv291@gmail.com>
2026-03-29 20:06:09 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/url_shortener");
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: String,
+  shortCode: String,
+  clicks: { type: Number, default: 0 },
+});
+
+const Url = mongoose.model("Url", urlSchema);
+
+app.post("/api/urls", async (req, res) => {
+  const newUrl = await Url.create({
+    originalUrl: req.body.originalUrl,
+    shortCode: req.body.shortCode,
+  });
+
+  res.json(newUrl);
+});
+
+app.get("/api/urls", async (req, res) => {
+  const urls = await Url.find();
+  res.json(urls);
+});
+
+app.get("/api/urls/:id", async (req, res) => {
+  const url = await Url.findById(req.params.id);
+  res.json(url);
+});
+
+app.get("/:shortCode", async (req, res) => {
+  const url = await Url.findOne({ shortCode: req.params.shortCode });
+
+  if (!url) {
+    return res.send("Not found");
+  }
+
+  url.clicks += 1;
+  await url.save();
+
+  res.redirect(url.originalUrl);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
